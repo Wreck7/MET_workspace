3c69f7bace6c4a9c85d60f061974ac43f6c969b6
Wreck7 <vishwagovula07@gmail.com>
2025-05-19 23:01:55 +0530

    git and github basics done 1st commit of medha

---
+++ day1.py
@@ -0,0 +1,5 @@
+# take user input - ask his marks for sci, social, maths
+# calculate the average of marks
+#  print the average in integer format
+
+
---
+++ day3.py
@@ -0,0 +1,20 @@
+# my_games= {"cricket", "football", "tennis"}
+# frd_games= {"cricket", "badminton", "chess"}
+# final= my_games.union(frd_games)
+# print(final)
+
+
+# my= {"cricket", "tennis", "football"}
+# frd= {"badminton", "cricket", "chess"}
+# find = my.difference(frd)
+# print(find)
+
+my_games = {"cricket", "football", "tennis"}
+my_games.clear()
+print(my_games)
+my_games.update({"cricket", "football", "tennis"})
+print(my_games)
+v=my_games
+print(v)
+
+
