43265fd2eb5795539917b7777d7af2fc8b2cdeb4
Wreck7 <vishwagovula07@gmail.com>
2025-06-19 23:41:40 +0530

    done!

--- url_shortener/backend/main.py
+++ url_shortener/backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from supabase import create_client
+import random
+import string
 
 url = 'https://trhagyasyglhhsmmngjx.supabase.co'
 api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyaGFneWFzeWdsaGhzbW1uZ2p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzNTQ3MzUsImV4cCI6MjA2NTkzMDczNX0.fgD8-UuOCV5hkCU5OdHMD2ROOvlsJqTIOzy1U6oLg2Q'
@@ -8,9 +10,19 @@ db = create_client(url, api_key)
 
 app = FastAPI()
 
-@app.get('/')
+def rand_str(length):
+    length = 8
+    phrase = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+    return phrase
+
+
+
+@app.post('/')
 def url_getter(long_url):
-    
+    res = db.table('urls').insert({"long_url": long_url, "phrase": rand_str(7)}).execute()
+    return res.data[0]['phrase']
+
+@app.get()
 
 
 
