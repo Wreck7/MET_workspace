284aa0daaba3f53de376ce436c5e38c47555f2da
Vishwa <vishwagovula07@gmail.com>
2025-07-31 15:48:37 +0530

    started typescript prac

---
+++ tsc_prac/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let username = 2;
+for (let index = 0; index < 5; index++) {
+    console.log(username * index);
+}
---
+++ tsc_prac/app.ts
@@ -0,0 +1,4 @@
+let username: number = 2
+for (let index = 0; index < 5; index++) {
+    console.log(username * index)
+}
