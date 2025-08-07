3b0b218fd0ede8568b77d6e0fc83a7a6a4ea300c
Vishwa <vishwagovula07@gmail.com>
2025-08-07 12:12:41 +0530

    CSS coompleted for the todolist

--- react/todolist/src/components/TodoList.jsx
+++ react/todolist/src/components/TodoList.jsx
@@ -6,11 +6,24 @@ const TodoList = ({ todos, toggleComplete, deleteTodo, getEditIndex }) => {
     <div>
       {todos.map((todo, i) => (
         <div
-          style={{ textDecoration: todo.completed ? "line-through" : "none" }} key={i}
+          style={{ textDecoration: todo.completed ? "line-through" : "none" }}
+          key={i}
+          className="todo-item"
         >
-          <span onClick={() => toggleComplete(i)}>{todo.text}</span>
-          <button onClick={() => deleteTodo(i)}>Delete</button>
-          <button onClick={() => getEditIndex(i)}>Edit</button>
+          <span
+            onClick={() => toggleComplete(i)}
+            className={`todo-text ${todo.completed ? "completed" : ""}`}
+          >
+            {todo.text}
+          </span>
+          <div className="todo-buttons">
+            <button onClick={() => getEditIndex(i)}>
+              <i class="fa-solid fa-pen-to-square"></i>
+            </button>
+            <button onClick={() => deleteTodo(i)}>
+              <i class="fa-solid fa-trash"></i>
+            </button>
+          </div>
         </div>
       ))}
     </div>
